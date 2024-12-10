53f0b05147f940411082103fb8779cf224f4c82c
Seckin Ozturk <franseck102@gmail.com>
2024-12-09 21:42:27 -0500

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import { Typography } from '@mui/material'
+import logo from "../assets/Logo.png"
 
 
 const Header = () => {
   return (
+    <div>
    <Typography color="error" align="center" variant="h2" component="h1">
     Task App with Typescript
+    <img src={logo} alt="" />
    </Typography>
+   </div>
   )
 }
 
